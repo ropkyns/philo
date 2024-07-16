aedf09744af9deac3ce69f72797295ed758d2676
Paul Martins Pereira <paulmart@made-f0Dr8s8.clusters.42paris.fr>
2024-07-16 16:39:00 +0200

    petit a petit

--- philo.h
+++ philo.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   philo.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: palu <palu@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: paulmart <paulmart@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/01 15:02:36 by palu              #+#    #+#             */
-/*   Updated: 2024/07/01 15:09:23 by palu             ###   ########.fr       */
+/*   Updated: 2024/07/16 11:23:21 by paulmart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,41 @@
 # include <pthread.h>
 # include <sys/time.h>
 # include <limits.h>
+
+typedef pthread_mutex_t	t_mutex;
+
+typedef struct s_data	t_data;
+
+typedef struct s_fork
+{
+	t_mutex	fork;
+	int		fork_index;
+}				t_fork;
+
+typedef struct s_philo
+{
+	int			index;
+	long		number_dinner;
+	bool		max_dinner;
+	long		last_diner_time;
+	t_fork		*lfork;
+	t_fork		*rfork;
+	pthread_t	thrd_id;
+}					t_philo;
+
+struct s_data
+{
+	long	nbr_philo;
+	long	t_to_die;
+	long	t_to_eat;
+	long	t_to_sleep;
+	long	limit_meal;
+	long	start_sim;
+	bool	end_sim;
+	t_fork	*forks;
+	t_philo	*philos;
+
+};
+
 
 #endif
